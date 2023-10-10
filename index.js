7491a07707ae4c2368a565168c4e0cc889b2caf5
Frank Totesaut <toteve@gmail.com>
2023-10-10 18:02:58 -0300

    actualizaciones varias index.js, env, rutas

--- index.js
+++ index.js
@@ -7,6 +7,50 @@ const app = express();
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// La clase Pool nos permite soportar multiconexiones y un mejor rendimiento en las consultas desde paquete pg
+import pkg from 'pg';
+const { Pool } = pkg;
+
+// definimos el objeto de conexion pool para una bd local
+/* const pool = new Pool({
+    host: 'localhost',  
+    user: 'postgres',
+    password: '1234',  
+    database: 'plan_de_viajes', 
+    port: 5433,
+    allowExitOnIdle: true  
+})
+ */
+
+// definimos el objeto de conexion pool para una bd remota desde render, si estamos en desarrollo se 
+// usa la externa, en produccion la interna, ambas las otorga RENDER
+/* const pool = new Pool({
+    connectionString:'postgres://tote:WlZnxlhCyYyNNQv9GfnxvYADGNLsj6zw@dpg-ckirgrcl4vmc73838nng-a.oregon-postgres.render.com/plan_de_viajes',
+    ssl: true
+
+})
+ */
+
+// si va a usar variables de entorno 
+// connectionString: process.env.DATABASE_URL_EXTERNA (EN DESARROLLO)
+/* const pool = new Pool({
+    connectionString: process.env.DATABASE_URL_EXTERNA,
+    ssl: true
+
+}) */
+
+// si va a usar variables de entorno 
+// connectionString: process.env.DATABASE_URL_INTERNA (en PRODUCCION))
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL_INTERNA,
+    ssl: true
+
+})
+
+
+
+
+
 // importar manejador de errores y modulos propios
 //import { handleErrors } from "./database/errors.js";
 //import { agregarViaje, verViajes, unViaje } from './database/consultas.js';
@@ -29,4 +73,10 @@ app.listen(PORT, () => {
 //0. GET para ver ruta raiz
 app.get("/", (req, res) => {
     res.json({ ok: true, result: "Acceso desde Render" });
+});
+
+//0. GET para obtener hora de una bd postgre desde render
+app.get("/hora", async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
 });
